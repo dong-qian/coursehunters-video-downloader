198b3d17a93ac7c261f1a2cb239c44a8b2a5d823
Prateek Thapa <psapien@ehrs-MacBook-Pro.local>
2019-06-16 17:24:54 +0545

    added lessonNumber to the lessonVideo

--- src/untils/video.js
+++ src/untils/video.js
@@ -34,10 +34,12 @@ const getCourseNamesAndURLS = async courseUrl => {
       el => el.attribs.class === 'lessons-name'
     );
 
-    filterNames.forEach(el => {
+    filterNames.forEach((el, idx) => {
       const videoName = el.children[0].data.replace(/[/:*?"<>|]/g, '');
       const name = videoName.replace(new RegExp('Урок', 'g'), 'Lesson');
-      lessonNames.push(name);
+      const number = idx + 1;
+
+      lessonNames.push(`${number}. ${name}`);
     });
 
     const filterLessonUrls = dataArray.filter(
